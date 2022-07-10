6d77061995baf2ceef036124bbf43f460e05508c
Deepak N <deepak.naresh@gmail.com>
2022-07-10 18:23:07 +0800

    remove display timer since it does not work

--- tables.js
+++ tables.js
@@ -14,29 +14,6 @@
 
 var the_stoptest
 
-function startTimer(duration, display) {
-	var timer = duration, minutes, seconds;
-	setInterval(function () {
-		minutes = parseInt(timer / 60, 10);
-		seconds = parseInt(timer % 60, 10);
-
-		minutes = minutes < 10 ? "0" + minutes : minutes;
-		seconds = seconds < 10 ? "0" + seconds : seconds;
-
-		display.textContent = minutes + ":" + seconds;
-
-		if (--timer < 0) {
-			timer = duration;
-		}
-	}, 1000);
-}
-
-window.onload = function () {
-	var seconds = miliseconds / 1000;
-	display = document.querySelector('#time');
-	startTimer(fiveMinutes, display);
-};
-
 
 (function () {
 	var num1;
